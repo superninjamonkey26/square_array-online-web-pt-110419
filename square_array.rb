e43ee5b885ef6b5bdc4919fef6da17810e5b055c
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-12 02:25:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  numbers = [1, 2, 3]
-  numbers.each do |array ** 2|
+  numbers = [integer]
+  numbers.each do |integer ** 2|
     puts "Number #{array}"
     return numbers
 end
